801c505b6c038df0b1304db4626a0e90743898e0
OutsydBaby <amichdot@gmail.com>
2026-03-14 12:33:58 +0100

    feat: added the dashboard layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+
+export default function DashboardLayout({children} : {children: ReactNode}) {
+    return(
+        <div>
+            {/* Sidebar */}
+            {/* Top Nav */}
+            {/* Main Content */}
+        </div>
+    )
+}

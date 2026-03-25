cfe1f90cfaf2924033cf5d0252947e5999dda68b
OutsydBaby <amichdot@gmail.com>
2026-03-25 16:09:36 +0100

    feat(badge): improved the styling and added icon

--- app/components/badge.tsx
+++ app/components/badge.tsx
@@ -1,11 +1,12 @@
+import { Wifi } from "lucide-react"
 export default function Badge({status}: {status?: "UP" | "DOWN"}) {
     const isUp = status === 'UP'
     return(
         <div>
             <span
-                className={` ${isUp ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'}`}
+                className={`flex items-center p-2 text-xs gap-2 rounded-lg ${isUp ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'}`}
             >
-                {isUp ? "UP" : "DOWN"}
+               <Wifi size={18}/> {isUp ? "UP" : "DOWN"}
             </span>
         </div>
     )

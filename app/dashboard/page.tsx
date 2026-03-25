dfeca533ce4610cb1c913ecaf17650cd671b8ec6
OutsydBaby <amichdot@gmail.com>
2026-03-25 16:01:41 +0100

    feat(Dashboard): moved Website info into a seperate component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Globe, Plus } from "lucide-react";
+import { Globe, Plus, RefreshCcw } from "lucide-react";
 import Modal from "@/app/components/modal";
 import { api } from "@/app/lib/api";
-import Badge from "@/app/components/badge";
+import WebsiteCard from "../components/websiteCard";
 
 type Website = {
   id: number;
@@ -28,59 +28,69 @@ export default function DashbordPage() {
     setWebsites((prev) => [...prev, newWebsite]);
   };
 
-  useEffect(() => {
-    const fetchWebsites = async () => {
-      try {
-        setLoading(true);
+  const fetchWebsites = async () => {
+    try {
+      setLoading(true);
 
-        const data = await api.get("/website/");
+      const data = await api.get("/website/");
 
-        const websitesWithStats = await Promise.all(
-          data.map(async (site: Website) => {
-            try {
-              const res = await api.get(`/website/${site.id}/stats`);
+      const websitesWithStats = await Promise.all(
+        data.map(async (site: Website) => {
+          try {
+            const res = await api.get(`/website/${site.id}/stats`);
 
-              return {
-                ...site,
-                status: res.stat?.lastCheck?.status || "DOWN",
-                responseTime: res.stat?.avgResponseTime || 0,
-                uptime: Number(res.stat?.upTimePercentage) || 0,
-                lastCheck: res.stat?.lastCheck?.checked_at || null,
-              };
-            } catch (error) {
-              return {
-                ...site,
-                status: "DOWN",
-              };
-            }
-          })
-        );
+            return {
+              ...site,
+              status: res.stat?.lastCheck?.status || "DOWN",
+              responseTime: res.stat?.avgResponseTime || 0,
+              uptime: Number(res.stat?.upTimePercentage) || 0,
+              lastCheck: res.stat?.lastCheck?.checked_at || null,
+            };
+          } catch (error) {
+            return {
+              ...site,
+              status: "DOWN",
+            };
+          }
+        })
+      );
 
-        setWebsites(websitesWithStats);
+      setWebsites(websitesWithStats);
       } catch (error) {
         console.log("Error fetching websites");
       } finally {
         setLoading(false);
       }
     };
+  
+    useEffect(() => {
+      fetchWebsites()
+    }, [])
 
-    fetchWebsites();
-  }, []);
+    
 
   return (
-    <div className="space-y-6">
-
+    <div className="w-full px-4 py-6 space-y-6">
       {/* Header */}
       <div className="flex justify-between items-center">
         <h2 className="text-2xl font-bold">Websites</h2>
 
-        <button
-          onClick={() => setIsOpen(true)}
-          className="flex items-center gap-2 bg-brand-primary text-white px-4 py-2 rounded-lg"
-        >
-          <Plus size={16} />
-          Add Website
-        </button>
+        <div className="flex items-center gap-1 sm:gap-2 md:gap-5">
+          <button
+            onClick={fetchWebsites}
+            disabled={loading}
+            className="hover:bg-brand-text-muted/30 p-2 rounded-lg transition disabled:opacity-50"
+          >
+            <RefreshCcw size={20} className={loading ? "animate-spin" : ""}/>
+          </button>
+          <button
+            onClick={() => setIsOpen(true)}
+            className="flex items-center gap-2 bg-brand-primary text-white px-4 py-2 rounded-lg"
+          >
+            <Plus size={16} />
+            Add Website
+          </button>
+        </div>
       </div>
 
       {/* Loading */}
@@ -116,60 +126,10 @@ export default function DashbordPage() {
 
       {/* Websites List */}
       {!loading && hasWebsites && (
-        <div className="bg-white rounded-xl border divide-y">
-
+        <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {websites.map((site) => (
-            <div
-              key={site.id}
-              className="p-5 flex justify-between items-center hover:bg-gray-50 transition"
-            >
-              {/* Left */}
-              <div>
-                <h3 className="font-semibold text-lg">
-                  {site.name}
-                </h3>
-                <p className="text-gray-500 text-sm">
-                  {site.url}
-                </p>
-              </div>
-
-              {/* Middle - Stats */}
-              <div className="flex items-center gap-8 text-sm">
-
-                <div>
-                  <p className="text-gray-500">Uptime</p>
-                  <p className="font-medium">
-                    {site.uptime ? `${site.uptime}%` : "N/A"}
-                  </p>
-                </div>
-
-                <div>
-                  <p className="text-gray-500">Response</p>
-                  <p className="font-medium">
-                    {site.responseTime
-                      ? `${Math.round(site.responseTime)} ms`
-                      : "N/A"}
-                  </p>
-                </div>
-
-                <div>
-                  <p className="text-gray-500">Last Check</p>
-                  <p className="font-medium">
-                    {site.lastCheck
-                      ? new Date(site.lastCheck).toLocaleString()
-                      : "N/A"}
-                  </p>
-                </div>
-
-              </div>
-
-              {/* Right - Status */}
-              <div>
-                <Badge status={site.status} />
-              </div>
-            </div>
+            <WebsiteCard key={site.id} site={site} />
           ))}
-
         </div>
       )}
 

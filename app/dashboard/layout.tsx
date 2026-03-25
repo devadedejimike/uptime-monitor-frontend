2a2ea8a9ed15c8272ac1f573f9b0927295c904a5
OutsydBaby <amichdot@gmail.com>
2026-03-25 16:06:23 +0100

    feat(Dashboard): simplified the layout and added avatar as user email's first leter

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Activity, Globe, LayoutDashboard, LogOut, Settings } from "lucide-react";
+import { Activity, LogOut} from "lucide-react";
 import Link from "next/link";
 import { ReactNode } from "react";
 import { UserProvider, useUser } from "../context/userContext";
@@ -20,20 +20,23 @@ const DashboardInner = ({children}: {children: ReactNode}) => {
     const {user} = useUser()
     return(
         <div className="min-h-screen bg-gray-50">
-            <header className="sticky top-0 z-50 flex items-center justify-between px-10 py-5 bg-white border-b border-gray-200">
-                <div className="flex items-center gap-3">
+            <header className="sticky top-0 z-50 flex items-center justify-between px-6 py-4 sm:px-10 lg:px-20 bg-white border-b border-gray-200">
+                <div className="flex items-center gap-1 sm:gap-3">
                     <Activity className="text-brand-primary "/>
                     <span className="font-bold text-lg">Uptime Watcher</span>
                 </div>
-                <div className="flex items-center gap-4 text-brand-text-muted">
-                    <p>{user?.email || "Unknown"}</p>
+                <div className="flex items-center gap-2 sm:gap-4 text-brand-text-muted">
+                    <div className="w-8 h-8 rounded-full bg-brand-primary text-white flex items-center justify-center text-sm">
+                        {user?.email?.[0].toUpperCase()}
+                    </div>
+                    <p className="hidden sm:block text-sm">{user?.email || "Unknown"}</p>
                     <Link href={''} className="flex items-center gap-1 hover:bg-brand-background hover:text-brand-primary p-2 rounded">
                         <LogOut size={18}/>
                     </Link>
                 </div>
             </header>
             {/* Main Content */}
-            <main className="flex-1 p-10 bg-brand-background/20">
+            <main className="py-6 px-4 sm:py-8 sm:px-6 md:px-12 lg:px-20 bg-brand-background/20">
                 {children}
             </main>
         </div>

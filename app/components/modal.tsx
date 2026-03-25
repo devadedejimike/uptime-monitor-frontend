27bef563cf4449993fc3b2a585c330f13fa4f12d
OutsydBaby <amichdot@gmail.com>
2026-03-25 16:08:51 +0100

    feat(modal): improved the ui and added onclick functions

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from "react";
 import { api } from "../lib/api";
+import { X } from "lucide-react";
 
 type ModalProps = {
     AddWebsite: (website: any) => void,
@@ -26,34 +27,45 @@ export default function Modal({AddWebsite, onClose}: ModalProps){
     }
 
     return(
-        <section className="fixed inset-0 bg-black/50 flex items-center justify-center">
-            <div className="bg-white p-6 rounded-lg w-[400px]">
-
-                <h3>Add a website to monitor</h3>
-                <p>We will check every 5 mins and track its uptime</p>
-
-                <form onSubmit={handleSubmitModal}>
+        <section className="fixed inset-0 bg-black/50 flex items-center justify-center" onClick={onClose}>
+            <div className="w-full bg-white max-w-md sm:max-w-lg p-6 rounded-lg space-y-5" onClick={(e) => {e.stopPropagation()}}>
+                <div className="flex items-center justify-between">
                     <div>
-                        <label htmlFor="name">Name</label>
+                        <h3 className="text-lg sm:text-xl font-semibold text-brand-primary">Add a website to monitor</h3>
+                        <p className="text-sm text-brand-text-muted">We&apos;ll check every 5 mins and track its uptime</p>
+                    </div>
+                    <div className="hover:bg-brand-primary hover:text-white text-brand-primary p-2 rounded-lg" onClick={onClose}>
+                        <X size={24}/>
+                    </div>
+
+                </div>    
+                    
+                <form onSubmit={handleSubmitModal} className="space-y-4">
+                    <div>
+                        <label htmlFor="name"  className="block text-sm font-medium mb-1">Name</label>
                         <input 
                             type="text"
                             value={name}
                             onChange={(e) => setName(e.target.value)}
-                            className="border w-full"
+                            placeholder="Example"
+                            className="w-full border border-gray-200 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-brand-primary"
+                            required
                         />
                     </div>
 
                     <div>
-                        <label htmlFor="url">URL</label>
+                        <label htmlFor="url" className="block text-sm font-medium">URL</label>
                         <input 
                             type="text"
                             value={url}
                             onChange={(e) => setUrl(e.target.value)}
-                            className="border w-full"
+                            placeholder="https://example.com"
+                            className="w-full border border-gray-200 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-brand-primary"
+                            required
                         />
                     </div>
 
-                    <button type="submit" className="mt-4 bg-black text-white px-4 py-2 rounded">
+                    <button type="submit" className="w-full bg-brand-primary text-white px-4 py-2 rounded">
                         Add Website
                     </button>
                 </form>

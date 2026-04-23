6e9c5a6c8efad1fd49a401e0fe41797fb088b716
OutsydBaby <amichdot@gmail.com>
2026-04-23 14:05:13 +0100

    created types

---
+++ app/lib/types.ts
@@ -0,0 +1,51 @@
+export type Card = {
+  id: string;
+  title: string;
+  description: string;
+};
+export type Baloon = {
+    id: string,
+    content: string
+}
+
+export const cards: Card[] = [
+  {
+    id: "deployment",
+    title: "After deployment",
+    description: "Make sure everything is still working after every release",
+  },
+  {
+    id: "apis",
+    title: "Running live APIs",
+    description: "Keep track of your production endpoints in real time",
+  },
+  {
+    id: "clients",
+    title: "Managing client sites",
+    description: "Monitor multiple client projects from one place",
+  },
+  {
+    id: "dashboards",
+    title: "Monitoring dashboards",
+    description: "Stay updated with system health at a glance",
+  },
+];
+
+export const Baloon = [
+    {
+        id: "1",
+        content: "Instant Downtime Alerts"
+    },
+    {
+        id: "2",
+        content: "Clear Response Time Tracking"
+    },
+    {
+        id: "3",
+        content: "Simple & Focused Dashboard"
+    },
+    {
+        id: "4",
+        content: "Reliable Monitoring Every 5 minutes"
+    }
+]
